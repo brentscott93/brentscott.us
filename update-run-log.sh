10b0366209af1bd9907a2387d8bed592a5d0f472
brentscott93 <bdscott@umass.edu>
2022-02-23 00:00:11 -0500

    cron auto-update run log

--- update-run-log.sh
+++ update-run-log.sh
@@ -4,4 +4,4 @@ git pull
 r -e "blogdown::build_site(local = FALSE, run_hugo = FALSE, build_rmd = 'content/run/2022/log.Rmd')"
 git add -A
 git commit -m "cron auto-update run log"
-git push
+git push origin master
